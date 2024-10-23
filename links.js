1844b09ab8c1c2ce5e272173328e3a05db920d57
alphanumeric-device <112668163+alphanumeric-device@users.noreply.github.com>
2024-10-22 19:01:27 -0500

    Update links.js
    commented out side nav functionality

--- links.js
+++ links.js
@@ -34,29 +34,29 @@ var links = {
    
     
   ],
-  "SideNav":[
-            {
-              "id": "item1",
-              "class": "doc_outline_sub top_nav-item top_nav-item-link",
-              "content": "Directors",
-              "href": "/outline/Directors.html",
-              "name": "Directors"
-            },
-            {
-              "id": "item2",
-              "class": "doc_outline_sub top_nav-item top_nav-item-link",
-              "content": "More Info",
-              "href": "/outline/Directors.html",
-              "name": "AAAAAAA"
-            },
-            {
-              "id": "item3",
-              "class": "doc_outline_sub top_nav-item top_nav-item-link",
-              "content": "How about that?",
-              "href": "/outline/Director.html",
-              "name": "AAAAAAA"
-            }
-  ],
+  // "SideNav":[
+  //           {
+  //             "id": "item1",
+  //             "class": "doc_outline_sub top_nav-item top_nav-item-link",
+  //             "content": "Directors",
+  //             "href": "/outline/Directors.html",
+  //             "name": "Directors"
+  //           },
+  //           {
+  //             "id": "item2",
+  //             "class": "doc_outline_sub top_nav-item top_nav-item-link",
+  //             "content": "More Info",
+  //             "href": "/outline/Directors.html",
+  //             "name": "AAAAAAA"
+  //           },
+  //           {
+  //             "id": "item3",
+  //             "class": "doc_outline_sub top_nav-item top_nav-item-link",
+  //             "content": "How about that?",
+  //             "href": "/outline/Director.html",
+  //             "name": "AAAAAAA"
+  //           }
+  // ],
   "Other (TBD)": {"TBD": "/outline/TBD.html"}
 }
 //export links
